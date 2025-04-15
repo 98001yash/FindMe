01c7d09784930506a795113e6b599013d0e72df2
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-15 17:50:09 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/company/FindMe/FindMeApplication.java
@@ -0,0 +1,13 @@
+package com.company.FindMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/FindMe/FindMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.FindMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
