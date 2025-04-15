28a56be07cebf52d9c3fd7175683e02a14c70d9b
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-15 18:41:56 +0530

    added the userDto

---
+++ src/main/java/com/company/FindMe/dtos/UserDto.java
@@ -0,0 +1,20 @@
+package com.company.FindMe.dtos;
+
+
+import com.company.FindMe.enums.Roles;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Set<Roles> roles;
+}
