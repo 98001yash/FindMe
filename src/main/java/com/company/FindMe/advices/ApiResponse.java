9548b405e2048e59753f764964afabce4a45a3e8
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-15 18:14:47 +0530

    added the ApiResponse class

--- src/main/java/com/company/FindMe/advices/ApiResponse.java
+++ src/main/java/com/company/FindMe/advices/ApiResponse.java
@@ -1,4 +1,29 @@
 package com.company.FindMe.advices;
 
-public class ApiResponse {
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    private LocalDateTime timeStamp;
+    private T data;
+    private ApiError error;
+
+
+    public ApiResponse(){
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data){
+        this();
+        this.data = data;
+    }
+
+    public ApiResponse(ApiError error){
+        this();
+        this.error = error;
+    }
 }
