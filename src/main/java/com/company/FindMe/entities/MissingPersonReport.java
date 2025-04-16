6e512509b297c9ca0e8e4b3b99c8fe516ca2f18a
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-16 12:40:05 +0530

    added the entity missingPersonReport

---
+++ src/main/java/com/company/FindMe/entities/MissingPersonReport.java
@@ -0,0 +1,37 @@
+package com.company.FindMe.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "missing_person_report")
+public class MissingPersonReport {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private Integer age;
+    private String gender;
+    private String lastLocation;
+    private String description;
+    private LocalDateTime missingSince;
+
+
+    @Enumerated(EnumType.STRING)
+    private ReportStatus status;
+    private String contactPhone;
+    private String contactEmail;
+    private Long createdByUserId;
+    private LocalDateTime createdAt;
+}
