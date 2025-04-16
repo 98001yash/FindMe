c92a30bae2ff092a838e5c50e36970246a34022e
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-16 12:46:08 +0530

    added the reportResponseDto

---
+++ src/main/java/com/company/FindMe/dtos/ReportResponseDto.java
@@ -0,0 +1,22 @@
+package com.company.FindMe.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportResponseDto {
+
+    private Long id;
+    private String fullName;
+    private Integer age;
+    private String gender;
+    private String lastSeenLocation;
+    private LocalDate missingPlace;
+    private String status;
+}
